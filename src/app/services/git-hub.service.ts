a7dd013fb0134b0ace30b92c201e3bb576df8dff
wdmuthiora <wdmuthiora@gmail.com>
2021-10-15 01:00:02 +0300

    Imported Observable from rxjs to github service.

--- src/app/services/git-hub.service.ts
+++ src/app/services/git-hub.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import {Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
