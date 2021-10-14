2e4d7020cee5074cd9f0c5eb0d77b85b5252acbb
wdmuthiora <wdmuthiora@gmail.com>
2021-10-15 01:11:10 +0300

    Configured the github service.

--- src/app/services/git-hub.service.ts
+++ src/app/services/git-hub.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -7,5 +7,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GitHubService {
 
-  constructor(private http:HttpClient ) { }
-}
+  constructor(private http: HttpClient) {
+  };
+  getData(): Observable<any> {
+    const url = "http://api.github.com/users"
+    
+    return this.http.get<any>(url)
+  
+
+  }
