5d59b8961d8a5b41dd3e3cfc249c67d9adcae6b1
wdmuthiora <wdmuthiora@gmail.com>
2021-10-18 00:30:21 +0300

    Populated the User class properties.

--- src/app/classes/user.spec.ts
+++ src/app/classes/user.spec.ts
@@ -1,7 +1,22 @@
-import { User } from './user';
-
-describe('User', () => {
-  it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
-  });
-});
+export class Repository {
+  constructor(
+    public login: string,
+    public id: number,
+    public node_id: string,
+    public avatar_url: string,
+    public gravatar_id: string,
+    public url: string,
+    public html_url: string,
+    public followers_url: string,
+    public following_url: string,
+    public gists_url: string,
+    public starred_url: string,
+    public subscriptions_url: string,
+    public organizations_url: string,
+    public repos_url: string,
+    public events_url: string,
+    public received_events_url: string,
+    public type: string,
+    public site_admin: boolean
+  ) {}
+}
